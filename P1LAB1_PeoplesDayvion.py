aab51dc1dfc5f22b3cd459d6dfcd7ba7fee3061d
Mysxtic <dayvionpeoples4@gmail.com>
2025-09-18 13:31:25 -0400

    Add files via upload

---
+++ P1LAB1_PeoplesDayvion.py
@@ -0,0 +1,8 @@
+# Dayvion Peoples
+# 9/18/25
+# P1LAB1
+# User enters first and lat name and "Welcome to CTI-110"
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello,", first_name, last_name + "!", "Welcome to CTI-110")
